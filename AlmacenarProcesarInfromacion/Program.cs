cb8151930c86ba5b332cc6d4dc19669c35818090
Atlast19 <ahleandro18@gmail.com>
2026-02-06 00:19:10 -0400

    Ejercicios BiDimencionales

---
+++ AlmacenarProcesarInfromacion/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace AlmacenarProcesarInfromacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Ingrese numero de filas: ");
+            int filas = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese numero de columnas: ");
+            int columnas = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[filas, columnas];
+
+            Console.WriteLine();
+            Console.WriteLine("Ingrese los valores de la matriz:");
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write($"Valor [{i},{j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Matriz ingresada:");
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            
+            int max = matriz[0, 0];
+            int min = matriz[0, 0];
+            int filaMax = 0, colMax = 0;
+            int filaMin = 0, colMin = 0;
+
+            
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    if (matriz[i, j] > max)
+                    {
+                        max = matriz[i, j];
+                        filaMax = i;
+                        colMax = j;
+                    }
+
+                    if (matriz[i, j] < min)
+                    {
+                        min = matriz[i, j];
+                        filaMin = i;
+                        colMin = j;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado");
+            Console.WriteLine($"Valor mayor: {max} en posicion [{filaMax},{colMax}]");
+            Console.WriteLine($"Valor menor: {min} en posicion [{filaMin},{colMin}]");
+        }
+    }
+}
---
+++ ProdemdioEstudiantes/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+namespace ProdemdioEstudiantes
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int estudiantes = 4;
+            int asignaturas = 3;
+
+            double[,] notas = new double[estudiantes, asignaturas];
+
+            
+            for (int i = 0; i < estudiantes; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Estudiante {i + 1}:");
+
+                for (int j = 0; j < asignaturas; j++)
+                {
+                    Console.Write($"Ingrese nota de asignatura {j + 1}: ");
+                    notas[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Promedio de cada estudiante:");
+
+            for (int i = 0; i < estudiantes; i++)
+            {
+                double suma = 0;
+
+                for (int j = 0; j < asignaturas; j++)
+                {
+                    suma += notas[i, j];
+                }
+
+                double promedio = suma / asignaturas;
+                Console.WriteLine($"Estudiante {i + 1}: {promedio}");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Promedio de cada asignatura:");
+
+            for (int j = 0; j < asignaturas; j++)
+            {
+                double suma = 0;
+
+                for (int i = 0; i < estudiantes; i++)
+                {
+                    suma += notas[i, j];
+                }
+
+                double promedio = suma / estudiantes;
+                Console.WriteLine($"Asignatura {j + 1}: {promedio}");
+            }
+        }
+    }
+}
